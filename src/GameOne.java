b67476a820a81fbeb342c6de68f40545456aad57
KokoroWJY <oumainoli@gmail.com>
2019-02-01 17:29:03 +0800

    你好世界!!

---
+++ src/GameOne.java
@@ -0,0 +1,7 @@
+public class GameOne {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++){
+            System.out.println("6666666666666666666666666666666666666666");
+        }
+    }
+}
